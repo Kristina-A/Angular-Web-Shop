5e8dac6a3e69fdf33017cc65c7493b8c0b42d14a
Kristina Antić <kristina.antic.II4@live.com>
2019-12-01 15:13:38 +0100

    check-out page

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../auth.service';
+import { OrderService } from './../order.service';
+import { Subscription } from 'rxjs';
+import { ShoppingCart } from './../models/shopping-cart';
+import { ShoppingCartService } from './../shopping-cart.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Order } from '../models/order';
 
 @Component({
-  selector: 'app-check-out',
+  selector: 'check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
+  shipping={};
+  cart:ShoppingCart;
+  userId:string;
+  cartSubscription:Subscription;
+  userSubscription:Subscription;
 
-  constructor() { }
+  constructor(private cartService:ShoppingCartService,
+     private orderService:OrderService,
+     private authService:AuthService){}
 
-  ngOnInit() {
+  async ngOnInit(){
+    let cart$=await this.cartService.getCart();
+    this.cartSubscription= cart$.subscribe(cart=>this.cart=cart);
+    this.userSubscription= this.authService.user$.subscribe(user=>this.userId=user.uid);
   }
 
+  ngOnDestroy(){
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
+  }
+
+  placeOrder(){
+    let order=new Order(this.userId, this.shipping, this.cart);
+    this.orderService.storeOrder(order);
+  }
 }
